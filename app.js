c8aeb0854b38f8b7304854f1afe19b53095b8951
e.hashemi <e.hashemi@elanus.ir>
2022-11-05 10:39:10 +0330

    simple calculator finished

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// alert("Hi")
+const display = document.querySelector(".display");
+const btns = document.getElementsByClassName("btn");
+const clearScreen = document.querySelector(".clear");
+function calculate(value){
+    return eval(value)
+}
+function clear () {
+    display.innerText = ""
+}
+for(let i = 0; i < btns.length; i++){
+    btns[i].addEventListener("click", (e) => {
+        display.innerText += e.target.innerText
+        switch (e.target.innerText) {
+            case "C":
+                clear()
+                break;
+            case "=":
+                let toCalculate = display.innerText.replace("=", "");
+                display.innerText =  calculate(toCalculate)
+                break;
+            default:
+                break;
+        }
+    })
+}
